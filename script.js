ff9800362915d3f09d5eefbd9c1ce63a01bf785c
Danish Sharma <24217002@edu.cbe.ab.ca>
2021-05-26 13:15:57 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const book2 = {
+    title: 'Book Two',
+    author: 'Nicholas Cage',
+    year: '2004',
+    getSummary: function() {
+        return `${this.title} was writted by ${this.author} in ${this.year}`;
+    }
+};
