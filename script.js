b6484dd76eeb75d5950534702f23b01339b9317c
Danish Sharma <24217002@edu.cbe.ab.ca>
2021-05-27 12:44:44 -0600

    stylesheet

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function Book() {
-    console.log('Book Initialized..');
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
 }
 
-const book1 = new Book();
+const book1 = new Book('Book One', 'Jane Doe', '2005');
+
+console.log(book1.author);
