5b8f9e082293e439cdc966d362b5afd1c3d56a05
Danish Sharma <24217002@edu.cbe.ab.ca>
2021-05-28 11:54:35 -0600

    classes & subclasses

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-//Constructor
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
+class Book {
+    constructor(title, author, year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getSummary(){
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
 }
-//getSummary prototype
-Book.prototype.getSummary = function(){
-    return `${this.title} was written by ${this.author} in ${this.year}`;
-};
-//magazine constructor
-function Magazine(title, author, year,month){
-    Book.call(this, title, author, year);
 
-    this.month = month;
+//Magazine Subclass
+class Magazine extends Book {
+    constructor(title, author, year, month) {
+        super(title, author, year);
+        this.month = month;
+    }
 }
-//Inheriting Prototype
-Magazine.prototype = Object.create(Book.prototype);
-//Instantiate Magazine Object
-const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Aug');
-//Magazine Constructor
-Magazine.prototype.constructor = Magazine;
-//console logging
+
+const mag1 = new Magazine('Magazine One', 'John Doe', '2018', 'Aug');
+
 console.log(mag1.getSummary());
-console.log(mag1);
-//at 26:03 of CrashCourse Video OOP
