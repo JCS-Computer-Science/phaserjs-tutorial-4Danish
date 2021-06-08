1550b7cbf30358f8be266d9fde7f725bc0fdc9d3
Danish Sharma <24217002@edu.cbe.ab.ca>
2021-06-08 13:55:00 -0600

    error

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,8 @@ import Phaser from '../lib/phaser.js'
 import Carrot from '../game/Carrot.js'
 
 export default class Game extends Phaser.Scene {
+    carrotsCollected = 0
+
     /** @type {Phaser.Physics.Arcade.Sprite} */
     player
 
@@ -14,6 +16,9 @@ export default class Game extends Phaser.Scene {
 
     /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
     cursors
+
+    /** @type {phaser.GameObjects.Text} */
+    carrotsCollectedText
 
     constructor(){
         super('game')
@@ -53,9 +58,18 @@ export default class Game extends Phaser.Scene {
         this.carrots = this.physics.add.group({
             classType: Carrot
         })
-        this.carrots.get(240, 320, 'carrot')
+        //this.carrots.get(240, 320, 'carrot')
 
         this.physics.add.collider(this.platforms, this.player)
+        this.physics.add.collider(this.platforms, this.carrots)
+
+        this.physics.add.overlap(
+            this.player,
+            this.carrots,
+            this.handleCollectCarrot,
+            undefined,
+            this
+        )
 
         this.player.body.checkCollision.up = false
         this.player.body.checkCollision.left = false
@@ -63,6 +77,11 @@ export default class Game extends Phaser.Scene {
 
         this.cameras.main.startFollow(this.player)
         this.cameras.main.setDeadzone(this.scale.width * 1.5)
+
+        const style = { color: '#002', fontSize: 24, fontFamily: "Cambria" }
+        this.carrotsCollectedText = this.add.text(240, 10, 'Carrots: 0', style)
+            .setScrollFactor(0)
+            .setOrigin(0.5, 0)
     }
 
     update(t, dt){
@@ -80,6 +99,8 @@ export default class Game extends Phaser.Scene {
             if (platform.y >= scrollY + 700){
                 platform.y = scrollY - Phaser.Math.Between(50,100)
                 platform.body.updateFromGameObject()
+
+                this.addCarrotAbove(platform)
             }
         })
         if (this.cursors.left.isDown && !touchingDown){
@@ -90,6 +111,12 @@ export default class Game extends Phaser.Scene {
             this.player.setVelocityX(0)
         }
         this.horizontalWrap(this.player)
+
+        const bottomPlatform = this.findBottomMostPlatform
+        ()
+        if(this.player.y > bottomPlatform.y + 200){
+            console.log('game over')
+        }
     }
 
     /**
@@ -105,5 +132,50 @@ export default class Game extends Phaser.Scene {
             sprite.x = -halfWidth
         }
     }
+    /**
+     * @param {Phaser.GameObjects.Sprite} sprite
+     */
+    addCarrotAbove(sprite){
+        const y = sprite.y - sprite.displayHeight
+
+        const carrot = this.carrots.get(sprite.x, y, 'carrot')
+
+        carrot.setActive(true)
+        carrot.setVisible(true)
+
+        this.add.existing(carrot)
+        carrot.body.setSize(carrot.width, carrot.height)
+
+        this.physics.world.enable(carrot)
+
+        return carrot
+    }
+    /**
+     * @param {Phaser.Physics.Arcade.Sprite} player
+     * @param {Carrot} carrot
+     */
+    handleCollectCarrot(player, carrot){
+        this.carrots.killAndHide(carrot)
+        this.physics.world.disableBody(carrot.body)
+
+        this.carrotsCollected++
+
+        const value = `Carrots: ${this.carrotsCollected}`
+        this.carrotsCollectedText.text = value
+    }
+
+    findBottomMostPlatform(){
+        const platforms = this.platforms.getChildren()
+        let bottonPlatform = platforms[0]
+
+        for (let i = 1; i < platforms.length; ++i){
+            const platform = platforms[i]
+            if (platform.y < bottomPlatform.y){
+                continue
+            }
+
+            bottomPlatform = platform
+        }
+        return bottomPlatform
+    }
 }
-//On part 7 of video series at 9:50.
